bf69e3b8b952eeb9e80eb196aeb3886008df8d15
Daniele Varrazzo <piro@gambitresearch.com>
2017-05-12 14:56:00 +0100

    Added test about basic row_filter functionality

--- tests/sql/row_filter.sql
+++ tests/sql/row_filter.sql
@@ -18,5 +18,27 @@ SELECT data FROM slot_get(
 SELECT data FROM slot_get(
 	'include', '{"table": "rf1", "where": "id >"}');
 
+-- Good config
+SELECT data FROM slot_get(
+	'include', '{"table": "rf1", "where": "whateva"}');
+SELECT data FROM slot_get(
+	'include', '{"table": "rf1", "where": "id > 0"}');
+
+-- Basic filtering
+INSERT INTO rf1 VALUES (1, 'foo', NULL);
+INSERT INTO rf1 VALUES (2, 'bar', '2017-01-01');
+INSERT INTO rf1 VALUES (3, 'baz', '2017-02-01');
+
+SELECT data FROM slot_peek(
+	'include', '{"table": "rf1", "where": "nofield = 42"}');
+SELECT data FROM slot_peek(
+	'include', '{"table": "rf1", "where": "id % 2 = 1"}');
+SELECT data FROM slot_peek(
+	'include', '{"table": "rf1", "where": "d is not null"}');
+SELECT data FROM slot_peek(
+	'include',
+	'{"table": "rf1", "where": "date_trunc(''month'', d) > ''2017-01-15''::date"}');
+SELECT data FROM slot_get(
+	'include', '{"table": "rf1", "where": "data ~ ''^ba.''"}');
 
 SELECT slot_drop();
