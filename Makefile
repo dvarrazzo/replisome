434800ec88dc0c5c5ebb127318f73698b3ca3f5c
Daniele Varrazzo <piro@gambitresearch.com>
2017-05-12 13:31:05 +0100

    Adding tests for row filter

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@ OBJS = src/replisome.o src/executor.o src/includes.o src/jsonbutils.o \
 
 REGRESS = --inputdir=tests \
 		init insert1 cmdline update1 update2 update3 update4 delete1 delete2 \
-		delete3 delete4 include repschema savepoint specialvalue toast bytea
+		delete3 delete4 include repschema row_filter savepoint specialvalue \
+		toast bytea
 
 PG_CONFIG = pg_config
 PGXS := $(shell $(PG_CONFIG) --pgxs)
---
+++ tests/sql/row_filter.sql
@@ -0,0 +1,20 @@
+\set VERBOSITY terse
+\pset format unaligned
+
+-- predictability
+SET synchronous_commit = on;
+
+DROP TABLE IF EXISTS rf1;
+
+CREATE TABLE rf1 (id int PRIMARY KEY, data text, d date);
+
+SELECT slot_create();
+
+-- Bad config
+SELECT data FROM slot_get(
+	'include', '{"where": "id > 0"}');
+SELECT data FROM slot_get(
+	'exclude', '{"table": "rf1", "where": "id > 0"}');
+
+
+SELECT slot_drop();
